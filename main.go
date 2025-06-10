58a2433147d70196db4f3f858c11bcf2a9bb3324
Jana S <jsivaraman@yahoo.com>
2025-06-10 18:09:48 -0400

    10-Jun-25: Variables

--- main.go
+++ main.go
@@ -3,5 +3,36 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Ninjas!")
+	//fmt.Println("Hello, Ninjas!")
+
+	// strings
+	// var nameOne string = "mario"
+	// var nameTwo = "luigi"
+	// var nameThree string
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// // reassigning variable
+	// nameOne = "peach"
+	// nameThree = "bowser"
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameFour := "yoshi" // short variable declaration. Can be used only for new variables. Can be used only inside functions.
+	// fmt.Println(nameFour)
+
+	// ints (positive and negative integers, no decimals)
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40 // short variable declaration
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// var numOne int8 = 25  // 8 bits. When specified, it can only hold values from -128 to 127.
+	// var numTwo int8 = -128
+	// var numThree uint8 = 255 // unsigned int, can only hold values from 0 to 255
+
+	// floats (decimal numbers). Need to specify memory (directly or indirectly).
+	// var priceOne float32 = 19.99 // 32 bits
+	// var priceTwo float64 = 296546545464654654654654654.99 // 64 bits, more precise
+	// scoreThree := 99.99 // short variable declaration, defaults to float64
 }
