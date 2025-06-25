0b17ea9eb40447249dec3f3b141c8142520e20bb
Jana S <jsivaraman@yahoo.com>
2025-06-24 20:03:56 -0400

    24-Jun-25: Strings, standard library

--- main.go
+++ main.go
@@ -1,106 +1,75 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+	"strings"
+)
+
 func main() {
-	// FIRST PROGRAM
-	//fmt.Println("Hello, Ninjas!")
 
-	// STRINGS
-	// var nameOne string = "mario"
-	// var nameTwo = "luigi"
-	// var nameThree string
+	// STRINGS PACKAGE
+	// greeting := "Hello there friends!"
 
-	// fmt.Println(nameOne, nameTwo, nameThree)
+	// fmt.Println(strings.Contains(greeting, "friends")) // true, checks if the string contains the substring "friends"
+	// fmt.Println(strings.Contains(greeting, "friends!!"))
+	// fmt.Println(strings.Contains(greeting, "Friends"))
 
-	// // reassigning variable
-	// nameOne = "peach"
-	// nameThree = "bowser"
+	// fmt.Println(strings.ToUpper(greeting)) // converts the string to uppercase
+	// fmt.Println(strings.Index(greeting, "th"))
+	// fmt.Println(strings.Split(greeting, " ")) // splits the string into a slice of strings using the space character as the delimiter
 
-	// fmt.Println(nameOne, nameTwo, nameThree)
+	// fmt.Println(strings.ReplaceAll(greeting, "friends", "ninjas")) // replaces all occurrences of "friends" with "ninjas". This returns a new string, it does not modify the original string.
+	// fmt.Println("the original string is still:", greeting)         // the original string is still unchanged
 
-	// nameFour := "yoshi" // short variable declaration. Can be used only for new variables. Can be used only inside functions.
-	// fmt.Println(nameFour)
+	// SORT PACKAGE
+	// ages := []int{20, 30, 10, 40, 50}
 
-	// ints (positive and negative integers, no decimals)
-	// var ageOne int = 20
-	// var ageTwo = 30
-	// ageThree := 40 // short variable declaration
-	// fmt.Println(ageOne, ageTwo, ageThree)
+	// index2 := sort.SearchInts(ages, 21)
+	// fmt.Println("the index of 21 is:", index2) // prints the index of the value 21 in the sorted slice. If searchInts is used on an unsorted slice, result will be incorrect or unexpected.
 
-	// var numOne int8 = 25  // 8 bits. When specified, it can only hold values from -128 to 127.
-	// var numTwo int8 = -128
-	// var numThree uint8 = 255 // unsigned int, can only hold values from 0 to 255
+	// sort.Ints(ages) // sorts the slice of integers in ascending order. Updates the original slice, does not return a new slice.
+	// fmt.Println(ages)
 
-	// floats (decimal numbers). Need to specify memory (directly or indirectly).
-	// var priceOne float32 = 19.99 // 32 bits
-	// var priceTwo float64 = 296546545464654654654654654.99 // 64 bits, more precise
-	// scoreThree := 99.99 // short variable declaration, defaults to float64
+	// index3 := sort.SearchInts(ages, 30)        // searches for the index of the value 30 in the sorted slice.
+	// fmt.Println("the index of 30 is:", index3) // prints the index of the value 30 in the sorted slice
 
-	//PRINT
-	// age := 20
-	// name := "Mario"
+	// index := sort.SearchInts(ages, 35)        // searches for the index of the value 30 in the sorted slice.
+	// fmt.Println("the index of 35 is:", index) // prints the index of the value 30 in the sorted slice
 
-	// fmt.Println("Hello, Ninjas!")
-	// fmt.Print("Hello, ")
-	// fmt.Print("World! \n")              // Print does not add a new line at the end
-	// fmt.Println("Hello again, Ninjas!") // Println adds a new line at the end
-	// fmt.Print("Hello again, everyone! \n")
+	// names := []string{"Alice", "Bob", "Charlie", "Dave", "Eve"}
+	// sort.Strings(names) // sorts the slice of strings in ascending order. Updates the original slice, does not return a new slice.
+	// fmt.Println("sorted names:", names)
 
-	// //Println with variables
-	// fmt.Println("Hello, Ninjas! My name is", name, "and I am", age, "years old.") // I don't need to add spaces between the variables and the strings, Println adds them automatically.
+	// fmt.Println("the index of 'Charlie' is:", sort.SearchStrings(names, "Charlie")) // searches for the index of the value "Charlie" in the sorted slice of strings
+	// fmt.Println("the index of 'Frank' is:", sort.SearchStrings(names, "Frank"))     // searches for the index of the value "Frank" in the sorted slice of strings
 
-	// // Printf (formatted strings) %_ = format specifier; does not add a new line at the end
-	// // %s = string, %d = integer, %f = float, %t = boolean
-	// // %v = any value, will automatically convert the value to a string
-	// // %T = type of the value
-	// // %q = quoted string
-	// // and many more, see https://pkg.go.dev/fmt#hdr-Printing
-	// fmt.Printf("Hello, Ninjas! My name is %v and I am %v years old.\n", name, age) // %v is a placeholder for any value. It will automatically convert the value to a string.
-	// fmt.Printf("Hello, Ninjas! My name is %q and I am %q years old.\n", name, age) // %q is a placeholder for a quoted string. It will automatically convert the value to a string and add quotes around it. Does not work with integers, so it will convert the integer to a string first.
-	// fmt.Printf("age is of type %T \n", age)                                        // %T is a placeholder for the type of the value. It will automatically convert the value to a string and print the type of the value.
-	// fmt.Printf("you scored %f points! \n", 25.555)
-	// fmt.Printf("you scored %0.1f points! \n", 25.55)
-	// fmt.Printf("you scored %0.2f points! \n", 25.555) // %.2f will print the float with 2 decimal places. It will round the number to 2 decimal places.
+	// STANDARD LIBRARY
+	greeting := "hello there friends!"
 
-	// // Sprintf (save formatted string to a variable)
-	// var message string = fmt.Sprintf("Hello, Ninjas! My name is %v and I am %v years old.", name, age) // Sprintf returns a formatted string, it does not print it to the console.
-	// fmt.Println("the saved string is:", message)                                                       // Print the formatted string
+	fmt.Println(strings.Contains(greeting, "hello"))
+	fmt.Println(strings.Contains(greeting, "bye"))
+	fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
+	fmt.Println(strings.ToUpper(greeting))
+	fmt.Println(strings.Index(greeting, "ll"))
+	fmt.Println(strings.Split(greeting, " "))
 
-	// ARRAYS AND SLICES
-	// arrays are fixed size, slices are dynamic size. Slices are arrays under the hood, but they are more flexible and easier to work with. Slices can grow and shrink in size, while arrays cannot.
-	// Will probably use slices most of the time, but arrays are still useful in some cases.
-	// arrays
-	// var ages[3] int = [3]int{20, 30, 40} // array of integers with a fixed size of 3
-	// var ages = [3]int{20, 30, 40} // slice of integers with a dynamic size. Slices are more flexible than arrays.
+	// the original value is unchanged
+	fmt.Println("original string value =", greeting)
 
-	// names := [4]string{"yoshi", "mario", "peach", "bowser"} // array of strings with a fixed size of 4
-	// names[1] = "luigi"                                      // reassigning a value in the array
-	// fmt.Println(ages, len(ages))
-	// fmt.Println(names, len(names))
+	ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
 
-	// // slices
-	// var scores []int = []int{100, 90, 80} // slice of integers with a dynamic size
-	// fmt.Println(scores, len(scores))
-	// scores = append(scores, 70) // append a value to the slice. Slices can grow in size. Arrays can't. Remember it needs to be assigned to a variable, does not automatically append.
-	// fmt.Println(scores, len(scores))
-	// scores[2] = 85 // reassigning a value in the slice
-	// fmt.Println(scores, len(scores))
-	// scores = append(scores, 60, 50, 40) // append multiple values to the slice
-	// fmt.Println(scores, len(scores))
+	sort.Ints(ages)
+	fmt.Println(ages) // original slice is changed
 
-	// //slice ranges. Picking a range of values from a slice to create a new slice.
-	// newScores := scores[1:4]               // create a new slice with values from index 1 to index 3 (not including index 4)
-	// fmt.Println(newScores, len(newScores)) // prints the new slice and its length
-	// newScores = scores[:3]                 // create a new slice with values from index 0 to index 2 (not including index 3)
-	// fmt.Println(newScores, len(newScores)) // prints the new slice and its length
-	// newScores = scores[2:]                 // create a new slice with values from index 2 to the end of the slice
-	// fmt.Println(newScores, len(newScores)) // prints the new slice and its length
-	// newScores = scores[:]                  // create a new slice with all values from the original slice
-	// fmt.Println(newScores, len(newScores)) // prints the new slice and its length
-	// newScores = scores[2:2]
-	// fmt.Println(newScores, len(newScores)) // prints the new slice and its length, which is an empty slice because the start and end index are the same
-	// // newScores = scores[2:1] // this will cause a runtime error because the start index is greater than the end index. Slices cannot be created with a start index greater than the end index.
-	// newScores = scores[2:8]                   // this will not cause a runtime error, but it will create a new slice with values from index 2 to the end of the slice, even if the end index is greater than the length of the slice.
-	// fmt.Println(newScores, len(newScores))    // prints the new slice and its length, which is an empty slice because the end index is greater than the length of the slice
-	// newScores = append(newScores, 30, 20, 10) // append values to the new slice
-	// fmt.Println(newScores, len(newScores))    // prints the new slice and its length after appending values
+	index := sort.SearchInts(ages, 30)
+	fmt.Println(index)
+
+	names := []string{"yoshi", "mario", "peach", "bowser", "luigi"}
+
+	sort.Strings(names)
+	fmt.Println(names)
+
+	fmt.Println(sort.SearchStrings(names, "bowser"))
+
 }
