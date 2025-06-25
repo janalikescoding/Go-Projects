bb73b675ea64c3410895683c2f5b2e7e9e23915d
Jana S <jsivaraman@yahoo.com>
2025-06-24 20:10:59 -0400

    24-Jun-25: Loops

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"fmt"
-	"sort"
-	"strings"
 )
 
 func main() {
@@ -44,32 +42,64 @@ func main() {
 	// fmt.Println("the index of 'Charlie' is:", sort.SearchStrings(names, "Charlie")) // searches for the index of the value "Charlie" in the sorted slice of strings
 	// fmt.Println("the index of 'Frank' is:", sort.SearchStrings(names, "Frank"))     // searches for the index of the value "Frank" in the sorted slice of strings
 
-	// STANDARD LIBRARY
-	greeting := "hello there friends!"
+	// // STANDARD LIBRARY
+	// greeting := "hello there friends!"
 
-	fmt.Println(strings.Contains(greeting, "hello"))
-	fmt.Println(strings.Contains(greeting, "bye"))
-	fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
-	fmt.Println(strings.ToUpper(greeting))
-	fmt.Println(strings.Index(greeting, "ll"))
-	fmt.Println(strings.Split(greeting, " "))
+	// fmt.Println(strings.Contains(greeting, "hello"))
+	// fmt.Println(strings.Contains(greeting, "bye"))
+	// fmt.Println(strings.ReplaceAll(greeting, "hello", "hi"))
+	// fmt.Println(strings.ToUpper(greeting))
+	// fmt.Println(strings.Index(greeting, "ll"))
+	// fmt.Println(strings.Split(greeting, " "))
 
-	// the original value is unchanged
-	fmt.Println("original string value =", greeting)
+	// // the original value is unchanged
+	// fmt.Println("original string value =", greeting)
 
-	ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
+	// ages := []int{45, 20, 35, 30, 75, 60, 50, 25}
 
-	sort.Ints(ages)
-	fmt.Println(ages) // original slice is changed
+	// sort.Ints(ages)
+	// fmt.Println(ages) // original slice is changed
 
-	index := sort.SearchInts(ages, 30)
-	fmt.Println(index)
+	// index := sort.SearchInts(ages, 30)
+	// fmt.Println(index)
 
-	names := []string{"yoshi", "mario", "peach", "bowser", "luigi"}
+	// names := []string{"yoshi", "mario", "peach", "bowser", "luigi"}
 
-	sort.Strings(names)
+	// sort.Strings(names)
+	// fmt.Println(names)
+
+	// fmt.Println(sort.SearchStrings(names, "bowser"))
+
+	// LOOPS
+	x := 0
+	for x < 5 {
+		fmt.Println("value of x is:", x)
+		x++ // infinite loop without this
+	}
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("value of i is:", i)
+	}
+
+	names := []string{"mario", "luigi", "yoshi", "peach"}
+
+	for i := 0; i < len(names); i++ {
+		fmt.Println(names[i])
+	}
+
+	for index, val := range names {
+		fmt.Printf("the value at pos %v is %v \n", index, val)
+		val = "new string"
+	}
+
+	for _, val := range names { // If you don't want to use the index, you can use the blank identifier _
+		// this will not change the original slice
+		// val is a copy of the value in the slice, not a reference to it
+		fmt.Print(val, ",")
+		val = "new string"
+	}
+
+	// changing val in a loop does not mutate the original slice
 	fmt.Println(names)
-
-	fmt.Println(sort.SearchStrings(names, "bowser"))
 
 }
