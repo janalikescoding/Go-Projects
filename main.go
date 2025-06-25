c71121317fb821185b7c26bf1f6a45b7ab81ebab
Jana S <jsivaraman@yahoo.com>
2025-06-24 20:18:48 -0400

    24-Jun-25: Booleans and conditionals

--- main.go
+++ main.go
@@ -70,36 +70,67 @@ func main() {
 
 	// fmt.Println(sort.SearchStrings(names, "bowser"))
 
-	// LOOPS
-	x := 0
-	for x < 5 {
-		fmt.Println("value of x is:", x)
-		x++ // infinite loop without this
+	// // LOOPS
+	// x := 0
+	// for x < 5 {
+	// 	fmt.Println("value of x is:", x)
+	// 	x++ // infinite loop without this
+	// }
+
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println("value of i is:", i)
+	// }
+
+	// names := []string{"mario", "luigi", "yoshi", "peach"}
+
+	// for i := 0; i < len(names); i++ {
+	// 	fmt.Println(names[i])
+	// }
+
+	// for index, val := range names {
+	// 	fmt.Printf("the value at pos %v is %v \n", index, val)
+	// 	val = "new string"
+	// }
+
+	// for _, val := range names { // If you don't want to use the index, you can use the blank identifier _
+	// 	// this will not change the original slice
+	// 	// val is a copy of the value in the slice, not a reference to it
+	// 	fmt.Print(val, ",")
+	// 	val = "new string"
+	// }
+
+	// // changing val in a loop does not mutate the original slice
+	// fmt.Println(names)
+
+	// BOOLEANS AND CONDITIONALS
+
+	age := 45
+
+	fmt.Println(age <= 50)
+	fmt.Println(age >= 50)
+	fmt.Println(age == 45)
+	fmt.Println(age != 50)
+
+	if age < 30 {
+		fmt.Println("age is less than 30")
+	} else if age < 40 {
+		fmt.Println("age is less than 40")
+	} else {
+		fmt.Println("age is not less than 40")
 	}
 
-	for i := 0; i < 5; i++ {
-		fmt.Println("value of i is:", i)
-	}
-
-	names := []string{"mario", "luigi", "yoshi", "peach"}
-
-	for i := 0; i < len(names); i++ {
-		fmt.Println(names[i])
-	}
+	names := []string{"mario", "luigi", "yoshi", "peach", "bowser"}
 
 	for index, val := range names {
+		if index == 1 {
+			fmt.Println("continuing at pos", index)
+			continue
+		}
+		if index > 2 {
+			fmt.Println("breaking at pos", index)
+			break
+		}
 		fmt.Printf("the value at pos %v is %v \n", index, val)
-		val = "new string"
 	}
-
-	for _, val := range names { // If you don't want to use the index, you can use the blank identifier _
-		// this will not change the original slice
-		// val is a copy of the value in the slice, not a reference to it
-		fmt.Print(val, ",")
-		val = "new string"
-	}
-
-	// changing val in a loop does not mutate the original slice
-	fmt.Println(names)
 
 }
